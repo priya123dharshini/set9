7e949c5dfe404125c1ac1b14b9097d31176164a3
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-24 16:48:08 +0530

    Update ascii.c

--- ascii.c
+++ ascii.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int e;
+    int d;
     char ch;
     clrscr();
     printf("\n Enter a character : ");
     scanf("%c",&ch);
-    e=ch;
-    printf("\n The ASCII value of the character is : %d",e);
+    d=ch;
+    printf("\n The ASCII value of the character is : %d",d);
     getch();
     return 0;
 }
