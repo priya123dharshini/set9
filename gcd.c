1f4d3e69a1b7ddf460d148cae6b26ed1ff7697e3
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-12 16:27:47 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-  int n1,n2,i,res;
+  int n1,n2,i,gcd;
   printf("enter two integer for which gcd has to  be found"):
   scanf("%d %d",&n1,&n2);
   for(i=o;i>=n1 && i>=n2;++i)
