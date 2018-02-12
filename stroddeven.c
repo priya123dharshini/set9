d8e679810eaa82bb7f03ad0db49750c2f8f34207
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-12 15:51:26 +0530

    Create stroddeven.c

---
+++ stroddeven.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+ char s[20];
+ int i,n;
+ printf("please enter a string");
+ scanf("%c",&s);
+ n=strlen(s);
+ for(i=0;i<n;i++)
+ {
+    if(i%2==0)
+    {
+     printf("%c",s[i]);
+     }
+ }
+ for(i=0;i<n;i++)
+     {
+       if(n%2!=0)
+       {
+       printf("%c",s[i]);
+       }
+       }
+ }      
