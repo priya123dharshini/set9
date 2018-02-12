e7d9b48d859ce1497467d14d4203b83f4277ac28
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-12 16:59:28 +0530

    Create dif.c

---
+++ dif.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  int k,o,i;
+   printf("enter number in kabbali and opponents team");
+    scanf("%d %d\n",&k,&o);
+    for(i=0;i!=eof;i++)
+     {
+       if(k>o)
+       {
+             printf("%d",k-o);
+        }
+        else
+        {
+           printf("%d",o-k);
+         }
+         }
+       }
+   
+    
