be159efc86b8d9390437460dd42c083d910feb7e
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-12 16:24:34 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int n1,n2,i,res;
+  printf("enter two integer for which gcd has to  be found"):
+  scanf("%d %d",&n1,&n2);
+  for(i=o;i>=n1 && i>=n2;++i)
+  {
+    if(n1%i==0 && n2%i==0)
+    {
+     i=gcd;
+     }
+   }
+   printf("%d",gcd);
+  }
