877b0bd947c8a89f2d54924cb10b8e47dfe06852
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-28 16:13:17 +0530

    Create kab.c

---
+++ kab.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int n,i,k,o;
+ printf("enter how many numbers you want");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+ printf("\nenter kabalis and opponents clan members");
+ scanf("%d %d",&k,&o);
+ printf("\n%d",k-o);
+ }}
