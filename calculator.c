93cecb96606674b127312a6b569850901f320185
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-28 16:00:54 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int n1,n2,res1,res2;
+ printf("enter two non negative integers");
+ scanf("%d %d",&n1,&n2);
+ res1=n1%n2;
+ res2=n1/n2;
+ printf("%d %d",res1,res2);
+ }
+ 
